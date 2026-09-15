90545d2e61f40f97577dfad6d906e06d1c46dedb
rafaikun558-alt <rafaikun558@gmail.com>
2026-09-15 11:45:02 +0700

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.HRIS_CONFIG = {
 
-  // URL Web App Apps Script
+  // URL Web App Apps Script TERBARU
   APPS_SCRIPT_URL:
-    'https://script.google.com/macros/s/AKfycbzyTjKCCLLVQMJrTQ74TWQ_kOzGYy0MvpXQkVqNjEGuMKEX5oQdRIncImX2cWgNBi_h4Q/exec',
+    'https://script.google.com/macros/s/AKfycbyMwhVMQ5LzmCMQOOL922mU7wtybZ6K4ezYNMKKHiptBj9ey0p46Uz9OhMCZ0mlvT73JQ/exec',
 
   // URL GitHub Pages
   APP_BASE_URL:
